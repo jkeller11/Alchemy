9ad37aff3dd978a707f7ae5fb2ae4d71c7b8e507
jkeller11 <98911883+jkeller11@users.noreply.github.com>
2025-02-02 11:46:47 -0500

    scale update

--- sketch_jan12b/sketch_jan12b.ino
+++ sketch_jan12b/sketch_jan12b.ino
@@ -1,4 +1,5 @@
 #include <Wire.h>
+#include <SparkFun_I2C_Mux_Arduino_Library.h>
 #define SDA_PIN 2  // Define custom SDA pin
 #define SCL_PIN 3  // Define custom SCL pin
 
@@ -9,28 +10,38 @@ void setup() {
 }
 
 void loop() {
-  sendData(0x60); //0x60 is weightx100 in big endian
-  printWeight();
-  //delay(500);
+  //Get transmission to start weighing
+  
+  //calibrate with bottles on then start getting weight until end tranmission is received
+  getWeight();
+  //tear();
+  //delay(1000);
+
+  
 }
 
-void sendData(byte data) {
-  Wire.beginTransmission(0x26);  // Address of the Scale
-  Wire.write(data);  // Send a byte
-  Wire.endTransmission();  // End transmission
-}
+void getWeight() {
+  Wire.beginTransmission(0x26);
+  Wire.write(0x60);
 
-void printWeight() {
   byte weightData[4];  //define empty array
   Wire.requestFrom(0x26, 16);  // Request 4 bytes from Scale
-  
+
   for(int x = 0; x < 4; x++){
     weightData[x] = Wire.read();  // Read byte
   }
 
-  float weight = (weightData[0] + (weightData[1] *256) + (weightData[2] *65536) + (weightData[3] *16777216))/100;
+  Wire.endTransmission();  // End transmission
+
+  int weight = (weightData[0] + (weightData[1] << 8) + (weightData[2] << 16) + (weightData[3] << 24))/100;
   Serial.print("Weight: ");
   Serial.print(weight);
   Serial.println("g");
-  
+}
+
+void tear(){
+  Wire.beginTransmission(0x26);  // Address of the Scale
+  Wire.write(0x50); //offset reg
+  Wire.write(1); //reset offset
+  Wire.endTransmission();  // End transmission
 }
