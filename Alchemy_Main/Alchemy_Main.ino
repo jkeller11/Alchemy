4e532c22be825b8fded646f5a0e4c4008b3f4a20
jkeller11 <98911883+jkeller11@users.noreply.github.com>
2025-09-11 18:21:55 -0400

    Real Progress
    - Modes added
    - Fixed Excel tag DB
    - Started adding screen layout

--- Alchemy_Main/Alchemy_Main.ino
+++ Alchemy_Main/Alchemy_Main.ino
@@ -7,6 +7,10 @@
 #include <P1AM.h>
 #include "Functions.h"
 
+#define relayCard = 1
+#define analogCard = 2
+#define pwmCard = 3
+
 // MAC Address for P1AM-200 PLC
 byte mac[] = { 0x60, 0x52, 0xD0, 0x07, 0xF5, 0x67 };
 
@@ -14,12 +18,11 @@ byte mac[] = { 0x60, 0x52, 0xD0, 0x07, 0xF5, 0x67 };
 IPAddress ip(192, 168, 1, 178);
 
 //Global Variables
-boolean MB_C[16];  //Modbus Coil Bits
-boolean MB_I[32];  //Modbus Input Bits
-int MB_HR[16];     //Modbus Holding Registers
-int MB_IR[16];     //Modbus Input Registers
+boolean MB_C[16];  //Modbus Coil Bits          R/W
+boolean MB_I[32];  //Modbus Input Bits         R       
+int MB_HR[16];     //Modbus Holding Registers  R
+int MB_IR[16];     //Modbus Input Registers    R/W
 int PumpDirectionPin = 3;
-int pumpDirection = 0;
 
 EthernetServer server(502);
 EthernetClient clients[8];
@@ -28,89 +31,109 @@ ModbusTCPServer modbusTCPServer;
 void setup() {
   while (!P1.init()){} ; //Wait for P1 Modules to Sign on   
   Ethernet.begin(mac, ip);
-  server.begin(); // start the server to begin listening
+  server.begin(); 
 
-
-  if (!modbusTCPServer.begin()) { // start the Modbus TCP server
-    while (1);
-  }
+  if (!modbusTCPServer.begin()) {while (1);} //Start the Modbus TCP server
 
   modbusTCPServer.configureCoils(0x00, 32);             //Coils
   modbusTCPServer.configureDiscreteInputs(0x00, 32);    //Discrete Inputs
   modbusTCPServer.configureHoldingRegisters(0x00, 16);  //Holding Register Words
   modbusTCPServer.configureInputRegisters(0x00, 16);    //Input Register Words
-
-  //pinMode(PumpDirectionPin, OUTPUT); //????
-
 }
 
 void loop() {
 
-  ModBusTCPService();
-
-  updateCoils();
-  updateInputs();
-  updateHoldingRegisters();
-  updateInputRegisters();
+  ModBusTCPService();        // Handles TCP Modbus connection to HMI
+  updateCoils();             // Updates Coil Array with values from Modbus server
+  updateInputs();            // Updates Input Array with values from Modbus server
+  updateHoldingRegisters();  // Updates HR Array with values from Modbus server
+  updateInputRegisters();    // Updates IR Array with values from Modbus server
   
-  //Saftey For Pump
-  if ((MB_C[7] == false) && (MB_HR[0] != 0)) { safteyPumpSpeed();}
+  //Check for Service Mode
+  if(MB_C[10]){
 
-  //Write Discrete outputs to P1-08TRS & P1-15CDD1
-  for (int i = 0; i < 8; i++) {
-    P1.writeDiscrete(MB_C[i], 1, i + 1);  //Data,Slot,Channel ... Channel is one-based.
   }
-  for (int i = 8; i < 15; i++) {
-    P1.writeDiscrete(MB_C[i], 2, i - 7);  //Data,Slot,Channel ... Channel is one-based.
+  
+  //Check for Engineering Mode
+  if(MB_C[11]){
+
   }
 
-  //Pump Speed
-  setPumpSpeed(MB_HR[0]);
+  //Check for Production Mode
+  if(MB_C[12]){
 
-  //Pump Direction
-  if (pumpDirection != MB_C[9]) {
-    pumpDirection = MB_C[9];
-    setPumpDirection(MB_C[9], MB_HR[0]);
+  }
+
+  // //Write Discrete outputs to relay card for Solenoids and Pump
+  // for (int i = 0; i <= 8; i++) {
+  //   P1.writeDiscrete(MB_C[i], 1, i + 1);  //Data,Slot,Channel ... Channel is one-based.
+  // }
+  // P1.writeDiscrete(MB_C[9], 2, 1); //Write Mixer State
+  
+  
+  // P1.writeDiscrete(MB_C[10], 2, 2); //Write Pump Direction
+  
+  // //Mixer and Pump Code
+  // setPumpSpeed(MB_HR[0]); //Pump Speed
+
+}
+///////////////////////////////////////Service Mode///////////////////////////////////////////////////////////////////////////////////////////////
+void ServiceMode(){
+  while(1){
+
+  }
+  reset();
+}
+///////////////////////////////////////Engineering Mode///////////////////////////////////////////////////////////////////////////////////////////
+void EngineeringMode(){//For troubleshooting purposes
+  while(1){
+    updateCoils();             // Updates Coil Array with values from Modbus server
+    updateInputs();            // Updates Input Array with values from Modbus server
+    updateHoldingRegisters();  // Updates HR Array with values from Modbus server
+    updateInputRegisters();    // Updates IR Array with values from Modbus server
+
+  }
+  reset();
+}
+///////////////////////////////////////Production Mode////////////////////////////////////////////////////////////////////////////////////////////
+void ProductionMode(){
+  while(1){
+
+  }
+  reset();
+}
+
+///////////////////////////////////////Equipment Functions////////////////////////////////////////////////////////////////////////////////////////
+void reset(){//set all Modbus data back to default
+  for(int x = 0; x <16; x++){
+    modbusTCPServer.coilWrite(x,0);
+    modbusTCPServer.holdingRegisterWrite(x,0);
+  }
+  
+  while(1){
+    updateInputs();
+    if(MB_I[0] == 1){ //check if E-Stop has been reset
+      break; 
+    }
   }
 }
 
 void setPumpSpeed(int speed) {
   int dutyCycle = map(speed, 0, 100, 255, 0);
 
-  //Serial1.write(byte(dutyCycle));
   P1.writePWM(dutyCycle, 20000, 3, 1);
   modbusTCPServer.holdingRegisterWrite(0, speed);
 }
 
-void safteyPumpSpeed() {
-  //Serial1.write((byte) 0x00);
-  P1.writePWM(0, 20000, 3, 1);
-  modbusTCPServer.holdingRegisterWrite(0, 0);
-}
 
 void setPumpDirection(int direction, int speed) {
   if (speed != 0) {
 
-    int y = speed / 4;
-    for (int x = 1; x <= 4; x++) {
-      setPumpSpeed(speed - (y * x));
-      delay(200);
-    }
-
-    setPumpSpeed(0);
-    delay(200);
-
-    digitalWrite(PumpDirectionPin, direction);
-
-    y = speed / 4;
-    for (int x = 1; x <= 4; x++) {
-      setPumpSpeed(y * x);
-      delay(200);
-    }
-    setPumpSpeed(speed);
-  } else {
-    digitalWrite(PumpDirectionPin, direction);
   }
+  else{
+
+  }
+
 }
 
 ////////////////////////////////////////Utility Functions/////////////////////////////////////////////////////////////////////////////////////////
